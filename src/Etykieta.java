3230cee97a0d0c755e253d6460c38b8980fa461e
LachTomasz <lach.tomasz@gmail.com>
2022-02-06 22:48:37 +0100

    First repo via IntelJ

---
+++ src/Etykieta.java
@@ -0,0 +1,9 @@
+public class Etykieta {
+    public static void main(String[] args) {
+        System.out.println("""
+                ******* Programowanie *******
+                *  obiektowe w języku Java  *
+                *    My name and surname    *
+                *****************************""");
+    }
+}
